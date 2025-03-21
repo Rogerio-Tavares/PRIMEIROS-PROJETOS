cfe8371134694dd15a44f416948232bad631620a
Rogério Tavares <oliveirarogerio585@gmail.com>
2025-03-21 13:19:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var SetaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("leonardo")
+var Bruna = window.document.getElementById("bruna")
+var Samantha = window.document.getElementById("samantha")
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    leonardo.style = "display: none"
+    samantha.style = "display: flex"
+    Setadireita.style = "display: none"
+    Setaesquerda.style ="display: flex; margin-top: 55px"
+}  
